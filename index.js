5bb67ee63bc76aa1e66ade4cddfbacb4c99a7b52
turzacse <turzacse@gmail.com>
2023-11-03 22:34:21 +0600

    add token and cookie

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app= express();
@@ -7,8 +9,12 @@ const app= express();
 const port = process.env.PORT || 5002;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 console.log(process.env.DB_USER);
 
@@ -24,6 +30,32 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+// our midleware 
+const logger = async(req, res, next) => {
+  console.log('called', req.host, req.originalUrl)
+  next();
+}
+
+const verifyToken = async(req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('token in midleware', token);
+  if(!token){
+    return res.status(401).send({message: 'not valid user'})
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      console.log(err);
+      return res.status(401).send({message: 'unathurized'})
+    }
+    console.log('value in the token', decoded);
+    req.user = decoded;
+    next();
+  })
+
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,7 +64,23 @@ async function run() {
     const servicesCollection = client.db('carDoctor').collection('services');
     const bookingCollection =client.db('carDoctor').collection('bookings');
 
-    app.get('/services', async(req, res) =>{
+
+    //auth api
+    app.post('/jwt', logger, async(req,res) =>{
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: false
+        // sameSite: 'none'
+      })
+      .send({success: true});
+    })
+
+    //services
+    app.get('/services', logger, async(req, res) =>{
         const cursor = servicesCollection.find();
         const result = await cursor.toArray();
         res.send(result);
@@ -51,8 +99,14 @@ async function run() {
 
     //bookings
 
-    app.get('/bookings', async(req, res) =>{
+    app.get('/bookings', logger, verifyToken, async(req, res) =>{
         console.log(req.query.email);
+        //console.log('token', req.cookies.token);
+        console.log('from valid token', req.user);
+
+        if(req.query.email !== req.user.email){
+          return res.status(403).send({message: 'forbidden access'})
+        }
         let query ={};
         if(req.query?.email) {
             query = {email: req.query.email}
@@ -66,6 +120,17 @@ async function run() {
         console.log(booking);
         const result = await bookingCollection.insertOne(booking);
         res.send(result);
+    })
+
+    app.put('/bookings/:id', async(req, res) =>{
+      const updateBokings = req.body;
+    })
+
+    app.delete('/bookings/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
     })
 
 
